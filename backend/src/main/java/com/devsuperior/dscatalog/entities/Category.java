8bec0c6b9f223047fb0e30bbc06b5d7804d80147
Weslley Palhares <weslley_palhares@outlook.com>
2025-02-12 00:47:56 -0300

    Database Seeding

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -10,7 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb-category")
+@Table(name = "tb_category")
 public class Category implements Serializable{
 	private static final long serialVersionUID = 1L;
 
